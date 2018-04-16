f4fa362592b101ac350f1c8ce51680e60aabe43f
choiys-94 <hcy3bkj@gmail.com>
2018-04-16 19:19:21 +0900

    add temporary frontend

--- ioc_server/app/routes.py
+++ ioc_server/app/routes.py
@@ -2,12 +2,37 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/icons')
+def icons():
+	return render_template('icons.html')
+
+@app.route('/map')
+def map():
+	return render_template('map.html')
+
+@app.route('/tables')
+def tables():
+	return render_template('tables.html')
+
+@app.route('/upgrade')
+def upgrade():
+	return render_template('upgrade.html')
+
+@app.route('/notifications')
+def notifications():
+	return render_template('notifications.html')
+
+@app.route('/typography')
+def typography():
+	return render_template('typography.html')
+
+@app.route('/user')
+def user():
+	return render_template('user.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+	app.run(host='0.0.0.0', debug=True)
