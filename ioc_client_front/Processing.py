7fe3be17f8f2e3fd8fd3c76a1b6b1b757f20c70d
chokim-01 <37997972+chokim-01@users.noreply.github.com>
2018-05-04 00:31:47 +0900

    just 3nd GUI
    Add btn . Pause - Continue etc 4 items add

--- ioc_client_front/Processing.py
+++ ioc_client_front/Processing.py
@@ -1,97 +1,101 @@
-# -*- coding: utf-8 -*-
+import sys
+from Completed import Ui_completed
 
-# Form implementation generated from reading ui file 'untitled1.ui'
-#
-# Created by: PyQt5 UI code generator 5.10.1
-#
-# WARNING! All changes made in this file will be lost!
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QDialog
+from PyQt5.QtGui import QIcon
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+class Ui_Processing(QDialog):
 
-class Ui_Processing(object): ## GUI 구성
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Process")
-        Dialog.resize(883, 481)
-        palette = QtGui.QPalette()
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(79, 87, 94))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Window, brush)
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(79, 87, 94))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Window, brush)
-        brush = QtGui.QBrush(QtGui.QColor(79, 87, 94))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(79, 87, 94))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Window, brush)
-        Dialog.setPalette(palette)
-        self.pushButton_P = QtWidgets.QPushButton(Dialog)
-        self.pushButton_P.setGeometry(QtCore.QRect(430, 40, 171, 81))
-        font = QtGui.QFont()
-        font.setFamily("Agency FB")
-        font.setPointSize(20)
-        self.pushButton_P.setFont(font)  ## pushButton_P = Pause. Pause -> continue toggle 예정
-        self.pushButton_P.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.pushButton_P.setStyleSheet("background-color:yellow")
-        self.pushButton_P.setObjectName("pushButton_P") 
-        self.pushButton_C = QtWidgets.QPushButton(Dialog)
-        self.pushButton_C.setGeometry(QtCore.QRect(640, 40, 171, 81))
-        font = QtGui.QFont()
-        font.setFamily("Agency FB")
-        font.setPointSize(20)
-        self.pushButton_C.setFont(font)  ## pushButton_C = Cancel. Cancel -> Start toggle 예정
-        self.pushButton_C.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.pushButton_C.setStyleSheet("background-color:brgb(21, 95, 255)")
-        self.pushButton_C.setObjectName("pushButton_C")
-        self.scrollArea = QtWidgets.QScrollArea(Dialog)
-        self.scrollArea.setGeometry(QtCore.QRect(40, 190, 771, 241))
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setObjectName("scrollArea")
-        self.scrollAreaWidgetContents = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 769, 239))
-        palette = QtGui.QPalette()
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Window, brush)
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Window, brush)
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Base, brush)
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255))
-        brush.setStyle(QtCore.Qt.SolidPattern)
-        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Window, brush)
-        self.scrollAreaWidgetContents.setPalette(palette)
-        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.checkBox = QtWidgets.QCheckBox(Dialog) ## Check Box. toggle 예정 - 검출목록 접기/펴기
-        self.checkBox.setGeometry(QtCore.QRect(40, 150, 96, 19)) 
-        self.checkBox.setObjectName("checkBox")
-        self.progressBar = QtWidgets.QProgressBar(Dialog)
-        self.progressBar.setGeometry(QtCore.QRect(70, 40, 241, 71))
-        self.progressBar.setProperty("value", 24)
-        self.progressBar.setObjectName("progressBar")
+    def __init__(self):
 
-        self.retranslateUi(Dialog) ## Dinamic Language Switchting
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        super().__init__()
 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Process"))
-        self.pushButton_P.setText(_translate("Dialog", "Pause"))
-        self.pushButton_C.setText(_translate("Dialog", "Cancel"))
-        self.checkBox.setText(_translate("Dialog", "검출 목록"))
+        self.processUI()
+
+    def processUI(self):
+        ### GUI size
+        self.setGeometry(1000,500,800,600)
+        self.setWindowTitle('Kx')
+        self.setStyleSheet("background-color: brgb(33, 33, 33)")
+
+        
+        ### GIF set
+        self.moviee = QLabel(self)
+        self.movie = QtGui.QMovie(".\\img\\Processing.gif")
+        self.moviee.setMovie(self.movie)
+        self.moviee.setGeometry(50,50,300,100)
+        self.movie.start()     
+
+        ### Pause Button
+        P_btn = QPushButton('',self)
+        P_btn.setIcon(QIcon('.\\img\\Pause.png'))
+        P_btn.setIconSize(QtCore.QSize(170,90))
+        P_btn.setFixedSize(150,70)
+        P_btn.move(420,50)
+
+        ### Continue Button
+        C_btn = QPushButton('',self)
+        C_btn.setIcon(QIcon('.\\img\\Continue.png'))
+        C_btn.setIconSize(QtCore.QSize(170,90))
+        C_btn.setFixedSize(150,70)
+        C_btn.move(420,50)
+
+        ### Cancel Button
+        Can_btn = QPushButton('',self)
+        Can_btn.setIcon(QIcon('.\\img\\Cancel.png'))
+        Can_btn.setIconSize(QtCore.QSize(170,90))
+        Can_btn.setFixedSize(150,70)
+        Can_btn.move(600,50)
+
+        ### Start Button
+
+        S_btn = QPushButton('',self)
+        S_btn.setIcon(QIcon('.\\img\\Start.png'))
+        S_btn.setIconSize(QtCore.QSize(170,90))
+        S_btn.setFixedSize(150,70)
+        S_btn.move(600,50)
+
+        ### hide btn P & S
+        C_btn.hide()
+        S_btn.hide()
+
+        ### Btn Clicked
+###        P_btn.clicked.connect(self.P_btn_clicked)
+###        C_btn.clicked.connect(self.C_btn_clicked)
+###        Can_btn.clicked.connect(self.Can_btn_clicked)
+###        S_btn.clicked.connect(self.S_btn_clicked)
+
+        self.show()
+
+       ### clicked btn event
+###    def P_btn_clicked(self, state):
+###        self.P_btn.setDisabled()
+###        self.C_btn.setEnabled(True)
+
+###    def C_btn_clicked(self):
+###       self.sender().setVisible(False)
+###       self.P_btn.setVisible(True)
+
+###    def Can_btn_clicked(self,state) :
+###        self.sender().setVisible(False)
+###        self.S_btn.setVisible(True)
+
+###    def S_btn_clicked(self,state) :
+###        self.sender().setVisible(False)
+###        self.Can_btn.setVisible(True)
+
+
+###    def search completed then open Completed GUI
+###        self.hide()
+#          self.UI = Ui_completed()
+###        UI.show()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    UI = Ui_Processing()
+    sys.exit(app.exec_())
+
+
+           ### 비활성 self.sender().setEnabled(False)
